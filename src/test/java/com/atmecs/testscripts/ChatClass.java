24b02d5c0d6593067d753bfdb6a1a6e6b86d1b73
ATMECS\Abuthahir.Musthafa <abuthahir.musthafa@atmecs.com>
2020-09-29 10:00:01 +0530

    Commit

---
+++ src/test/java/com/atmecs/testscripts/ChatClass.java
@@ -0,0 +1,52 @@
+package com.atmecs.testscripts;
+
+import java.io.IOException;
+import java.util.Properties;
+import java.util.concurrent.TimeUnit;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.JavascriptExecutor;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.Select;
+import org.openqa.selenium.support.ui.WebDriverWait;
+import org.testng.annotations.DataProvider;
+import org.testng.annotations.Test;
+
+import com.atmecs.browser.SetUp;
+import com.atmecs.constant.ConstantClass;
+import com.atmecs.helper.HelperClass;
+import com.atmecs.reader.PropertyReader;
+import com.atmecs.testdata.DataProviderClass;
+import com.atmecs.testdata.XlsReader;
+
+import sun.awt.SunHints.Value;
+
+public class ChatClass extends SetUp {
+
+	Properties locators;
+
+	@Test(dataProvider = "Data", dataProviderClass = DataProviderClass.class)
+
+	public void startChat(String name, String email, String whatsapp) throws InterruptedException {
+		locators = PropertyReader.readProperty(ConstantClass.LOCATORS_PATH);
+
+		driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
+
+		driver.switchTo().frame(locators.getProperty("Frame_Id"));
+		WebDriverWait wait = new WebDriverWait(driver, 40);
+		wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath(locators.getProperty("ChatWidget"))));
+
+		HelperClass.jsClick(driver, locators.getProperty("ChatWidget"));
+		HelperClass.sendKeyById(driver, locators.getProperty("NameData"), name);
+		HelperClass.ElementSendKeys(driver, locators.getProperty("WhatsApp"), whatsapp);
+		HelperClass.sendKeyById(driver, locators.getProperty("EmailData"), email);
+
+		HelperClass.selectFromDropDown(driver, locators.getProperty("Dropdown"), locators.getProperty("value"));
+		HelperClass.jsClick(driver, locators.getProperty("StartChat"));
+
+		driver.switchTo().defaultContent();
+
+	}
+
+}
